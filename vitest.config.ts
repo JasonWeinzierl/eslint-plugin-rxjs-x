2071126c3ccee64790471965544d7f467de76a76
Jason Weinzierl <weinzierljason@gmail.com>
2024-11-01 13:45:17 -0500

    chore: add code coverage and github-actions reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    test: {
-        globals: true,
-        reporters: 'dot',
+  test: {
+    globals: true,
+    reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
+    coverage: {
+      reporter: ['text-summary', 'lcovonly'],
     },
+  },
 });
