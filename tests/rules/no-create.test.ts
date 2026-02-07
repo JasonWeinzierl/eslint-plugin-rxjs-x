093383b78f96742a7f47e3e7d4b0e4cc2cef8946
Jason Weinzierl <weinzierljason@gmail.com>
2026-02-07 10:25:43 -0600

    feat(no-create): forbid Subject.create

--- tests/rules/no-create.test.ts
+++ tests/rules/no-create.test.ts
@@ -4,7 +4,32 @@ import { fromFixture } from '../etc';
 import { ruleTester } from '../rule-tester';
 
 ruleTester({ types: true }).run('no-create', noCreateRule, {
-  valid: [],
+  valid: [
+    stripIndent`
+      // new Observable
+      import { Observable, Observer } from "rxjs";
+
+      const ob = new Observable((observer: Observer<string>) => {
+        observer.next("Hello, world.");
+        observer.complete();
+        return () => {};
+      });
+    `,
+    stripIndent`
+      // Subject
+      import { Subject, of } from "rxjs";
+
+      const sub = new Subject();
+    `,
+    stripIndent`
+      // unrelated create
+      const Subject = {
+        create() { }
+      }
+      
+      Subject.create();
+    `,
+  ],
   invalid: [
     fromFixture(
       stripIndent`
@@ -17,6 +42,42 @@ ruleTester({ types: true }).run('no-create', noCreateRule, {
             observer.complete();
             return () => {};
         });
+      `,
+    ),
+    fromFixture(
+      stripIndent`
+        // Subject.create
+        import { Subject, of } from "rxjs";
+
+        const sub = Subject.create(new Subject(), of(1, 2, 3));
+                            ~~~~~~ [forbiddenSubject]
+      `,
+    ),
+    fromFixture(
+      stripIndent`
+        // BehaviorSubject.create
+        import { BehaviorSubject, Subject, of } from "rxjs";
+
+        const sub = BehaviorSubject.create(new Subject(), of(1, 2, 3));
+                                    ~~~~~~ [forbiddenSubject]
+      `,
+    ),
+    fromFixture(
+      stripIndent`
+        // AsyncSubject.create
+        import { AsyncSubject, Subject, of } from "rxjs";
+
+        const sub = AsyncSubject.create(new Subject(), of(1, 2, 3));
+                                 ~~~~~~ [forbiddenSubject]
+      `,
+    ),
+    fromFixture(
+      stripIndent`
+        // ReplaySubject.create
+        import { ReplaySubject, Subject, of } from "rxjs";
+
+        const sub = ReplaySubject.create(new Subject(), of(1, 2, 3));
+                                  ~~~~~~ [forbiddenSubject]
       `,
     ),
   ],
