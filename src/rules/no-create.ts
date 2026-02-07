5a771aaa5605e40cd827a9508cc487c581c7625d
Jason Weinzierl <weinzierljason@gmail.com>
2026-02-07 10:25:43 -0600

    feat(no-create): forbid Subject.create

--- src/rules/no-create.ts
+++ src/rules/no-create.ts
@@ -6,19 +6,20 @@ export const noCreateRule = ruleCreator({
   defaultOptions: [],
   meta: {
     docs: {
-      description: 'Disallow the static `Observable.create` function.',
+      description: 'Disallow the static `Observable.create` and `Subject.create` functions.',
       recommended: 'recommended',
       requiresTypeChecking: true,
     },
     messages: {
       forbidden: 'Observable.create is forbidden; use new Observable.',
+      forbiddenSubject: 'Subject.create is forbidden.',
     },
     schema: [],
     type: 'problem',
   },
   name: 'no-create',
   create: (context) => {
-    const { couldBeObservable } = getTypeServices(context);
+    const { couldBeObservable, couldBeType } = getTypeServices(context);
 
     return {
       'CallExpression > MemberExpression[object.name=\'Observable\'] > Identifier[name=\'create\']':
@@ -27,6 +28,16 @@ export const noCreateRule = ruleCreator({
           if (couldBeObservable(memberExpression.object)) {
             context.report({
               messageId: 'forbidden',
+              node,
+            });
+          }
+        },
+      'CallExpression > MemberExpression[object.name=/^(Subject|BehaviorSubject|AsyncSubject|ReplaySubject)$/] > Identifier[name=\'create\']':
+        (node: es.Identifier) => {
+          const memberExpression = node.parent as es.MemberExpression;
+          if (couldBeType(memberExpression.object, /^(Subject|BehaviorSubject|AsyncSubject|ReplaySubject)$/)) {
+            context.report({
+              messageId: 'forbiddenSubject',
               node,
             });
           }
