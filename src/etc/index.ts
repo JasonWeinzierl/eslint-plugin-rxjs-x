3fb7eb6d4bcdf6e790ff83a25bffa2dd504ec81f
Jason Weinzierl <weinzierljason@gmail.com>
2026-02-16 16:58:19 -0600

    fix: remove scope-manager dependency (#379)
    scope-manager is still a transitive dependency via utils. But this
    change lets us explicitly remove it.
    
    Only one rule uses the `isImport` utility, which is unique compared to
    every other rule. It should be fine to just check if `takeWhile` is
    getting called from within a `pipe` function and assume it's RxJS.

--- src/etc/index.ts
+++ src/etc/index.ts
@@ -3,5 +3,4 @@ export * from './could-be-type';
 export * from './find-parent';
 export * from './get-loc';
 export * from './get-type-services';
-export * from './is-import';
 export * from './is';
